278c6c040ac370adae30ffb471ca793d1a3d4324
Nasstiy <bachilo0412@gmail.com>
2024-05-26 16:26:31 +0500

    base url

--- app/api/config.js
+++ app/api/config.js
@@ -1,7 +1,7 @@
-export const BASE_URL = 'https://pindie-backend-nasty.nomoredomainswork.ru/api/'
+export const BASE_URL = 'https://pindie-backend-nasty.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
-  me: `${BASE_URL}/me`,
+  me: `${BASE_URL}/users/me`,
 }
